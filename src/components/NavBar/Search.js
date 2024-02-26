8ad295deb551767f0b115d568a81f1efde39c616
VladyslavPetruk96 <petrukvladyslav96@gmail.com>
2024-02-26 20:06:27 +0200

    + focus on search bar on Enter key fc

--- src/components/NavBar/Search.js
+++ src/components/NavBar/Search.js
@@ -1,4 +1,23 @@
+import { useEffect } from 'react';
+import { useRef } from 'react';
+
 export default function Search({ query, setQuery }) {
+  const searchBarEl = useRef(null);
+
+  useEffect(() => {
+    function callback(e) {
+      if (document.activeElement === searchBarEl.current) return;
+
+      if (e.code === 'Enter') {
+        searchBarEl.current.focus();
+        setQuery('');
+      }
+    }
+    document.addEventListener('keydown', callback);
+
+    return () => document.removeEventListener('keydown', callback);
+  }, [setQuery]);
+
   return (
     <input
       className="search"
@@ -6,6 +25,7 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={e => setQuery(e.target.value)}
+      ref={searchBarEl}
     />
   );
 }
